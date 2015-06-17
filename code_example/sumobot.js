87890d9e7db9d9109011d91713cca1c86b19e23a
Linda Nichols <lynnaloo@gmail.com>
2015-06-16 23:40:24 -0400

    Eslint cleanup and added rover.js

--- code_example/sumobot.js
+++ code_example/sumobot.js
@@ -2,71 +2,67 @@
 // Sumobot Jr demo program
 // =======================
 
-var five = require("johnny-five");
+'use strict';
+
+var five = require('johnny-five');
 var keypress = require('keypress');
 
-keypress(process.stdin);
 
 var board = new five.Board();
 
-board.on("ready", function() {
+board.on('ready', function() {
 
-  console.log("Welcome to Sumobot Jr!")
-  console.log("Control the bot with the arrow keys, and SPACE to stop.")
+  console.log('Welcome to Sumobot Jr!');
+  console.log('Control the bot with the arrow keys, and SPACE to stop.');
 
-  var left_wheel  = new five.Servo({ pin:  9, type: 'continuous' }).stop();
-  var right_wheel = new five.Servo({ pin: 10, type: 'continuous'  }).stop();
+  var leftWheel = new five.Servo({ pin: 9, type: 'continuous' }).stop();
+  var rightWheel = new five.Servo({ pin: 10, type: 'continuous' }).stop();
 
 
-  process.stdin.resume(); 
-  process.stdin.setEncoding('utf8'); 
-  process.stdin.setRawMode(true); 
-
+  keypress(process.stdin);
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
   process.stdin.on('keypress', function (ch, key) {
-    
-    if ( !key ) return;
 
+    if ( !key ) { return; }
 
-    if ( key.name == 'q' ) {
+    if ( key.name === 'q' ) {
 
       console.log('Quitting');
       process.exit();
 
-    } else if ( key.name == 'up' ) {
+    } else if ( key.name === 'up' ) {
 
       console.log('Forward');
-      left_wheel.ccw();
-      right_wheel.cw();
+      leftWheel.ccw();
+      rightWheel.cw();
 
-    } else if ( key.name == 'down' ) {
+    } else if ( key.name === 'down' ) {
 
       console.log('Backward');
-      left_wheel.cw();
-      right_wheel.ccw();      
+      leftWheel.cw();
+      rightWheel.ccw();
 
-    } else if ( key.name == 'left' ) {
+    } else if ( key.name === 'left' ) {
 
       console.log('Left');
-      left_wheel.ccw();
-      right_wheel.ccw();      
+      leftWheel.ccw();
+      rightWheel.ccw();
 
 
-    } else if ( key.name == 'right' ) {
+    } else if ( key.name === 'right' ) {
 
       console.log('Right');
-      left_wheel.cw();
-      right_wheel.cw();
+      leftWheel.cw();
+      rightWheel.cw();
 
-    } else if ( key.name == 'space' ) {
+    } else if ( key.name === 'space' ) {
 
       console.log('Stopping');
-      left_wheel.stop();
-      right_wheel.stop();
+      leftWheel.stop();
+      rightWheel.stop();
 
     }
-
-
   });
-
-
 });
