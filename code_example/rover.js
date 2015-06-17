57eaeb86ef5ea441f8d5f75d985b806382e7bd7c
Linda Nichols <lynnaloo@gmail.com>
2015-06-16 23:55:28 -0400

    Improvements based on Johnny-Five API

--- code_example/rover.js
+++ code_example/rover.js
@@ -4,50 +4,47 @@ var five = require('johnny-five');
 var board = new five.Board();
 var keypress = require('keypress');
 
-// Use your shield configuration from the list
-// http://johnny-five.io/api/motor/#pre-packaged-shield-configs
-var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V1;
-var motors = new five.Motors([
-  configs.M1,
-  configs.M2
-]);
-
-function forward() {
-  console.log('Going forward');
-  motors.fwd();
-}
-
-function backward() {
-  console.log('Going backward');
-  motors.rev();
-}
-
-function left() {
-  console.log('Going left');
-  motors[0].rev();
-  motors[1].fwd();
-}
-
-function right() {
-  console.log('Going right');
-  motors[1].rev();
-  motors[0].fwd();
-}
-
-function stop() {
-  motors.stop();
-}
-
 board.on('ready', function() {
+  // Use your shield configuration from the list
+  // http://johnny-five.io/api/motor/#pre-packaged-shield-configs
+  var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V1;
+  var motors = new five.Motors([
+    configs.M1,
+    configs.M2
+  ]);
 
   this.repl.inject({
     motors: motors
   });
 
-  motors.speed(255);
-
   console.log('Welcome to the Pee Wee Runt Rover!');
   console.log('Control the bot with the arrow keys, and SPACE to stop.');
+
+  function forward() {
+    console.log('Going forward');
+    motors.fwd(255);
+  }
+
+  function backward() {
+    console.log('Going backward');
+    motors.rev(255);
+  }
+
+  function left() {
+    console.log('Going left');
+    motors[0].rev(200);
+    motors[1].fwd(200);
+  }
+
+  function right() {
+    console.log('Going right');
+    motors[1].rev(200);
+    motors[0].fwd(200);
+  }
+
+  function stop() {
+    motors.stop();
+  }
 
   keypress(process.stdin);
   process.stdin.resume();
